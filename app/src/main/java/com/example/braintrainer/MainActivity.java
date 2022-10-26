692ff97755c7ea7aaa6be41f6c47262a245f9106
Vyacheslav Nikitin <space.face.ru@gmail.com>
2022-10-26 16:11:26 +0300

    #7 created loading new answer when push button and created toasts

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -6,17 +6,22 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.preference.PreferenceManager;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textViewQuestion;
     private TextView textViewTimer;
     private TextView textViewScore;
     private TextView textViewOpinion0;
     private TextView textViewOpinion1;
     private TextView textViewOpinion2;
     private TextView textViewOpinion3;
+    private ArrayList<TextView> options = new ArrayList<>();
 
     private String question;
     private int rightAnswer;
@@ -30,12 +35,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textViewQuestion = findViewById(R.id.textViewQuestion);
         textViewTimer = findViewById(R.id.textViewTimer);
         textViewScore = findViewById(R.id.textViewScore);
         textViewOpinion0 = findViewById(R.id.textViewOpinion0);
         textViewOpinion1 = findViewById(R.id.textViewOpinion1);
         textViewOpinion2 = findViewById(R.id.textViewOpinion2);
         textViewOpinion3 = findViewById(R.id.textViewOpinion3);
+
+        options.add(textViewOpinion0);
+        options.add(textViewOpinion1);
+        options.add(textViewOpinion2);
+        options.add(textViewOpinion3);
+        playNext();
+    }
+
+    private void playNext(){
+        for (int i = 0; i < options.size(); i++){
+            if (i == rightAnswerPosition){
+                options.get(i).setText(Integer.toString(rightAnswer));
+            } else {
+                options.get(i).setText(Integer.toString(generateWrongAnswer()));
+            }
+        }
     }
 
     private void generationQuestion(){
@@ -50,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
             rightAnswer = a-b;
             question = String.format("%s - %s", a, b);
         }
+        textViewQuestion.setText(question);
         rightAnswerPosition =(int) (Math.random()*4);
     }
 
@@ -59,5 +82,17 @@ public class MainActivity extends AppCompatActivity {
             result = (int) (Math.random()*max*2+1) - (max-min);
         } while (result == rightAnswer);
         return result;
+    }
+
+    public void onClickAnswer(View view){
+        TextView textView = (TextView) view;
+        String answer = textView.getText().toString();
+        int chosenAnswer = Integer.parseInt(answer);
+        if (chosenAnswer == rightAnswer) {
+            Toast.makeText(this, "Верно", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Неверно", Toast.LENGTH_SHORT).show();
+        }
+        playNext();
     }
 }
