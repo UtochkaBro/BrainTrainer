12cf0f243a0ae985f71f95f53ba676a3f5cd98c1
Vyacheslav Nikitin <space.face.ru@gmail.com>
2022-10-26 09:38:43 +0300

    #2 start used SharedPreferences

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.braintrainer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        preferences.edit().putInt("test", 5).apply();
+        int test = preferences.getInt("test", 0);
+        Toast.makeText(this, Integer.toString(test), Toast.LENGTH_SHORT).show();
     }
 }
