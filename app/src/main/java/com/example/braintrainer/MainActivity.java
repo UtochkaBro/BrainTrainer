82d021c0775f97eab9ad0c2ae8d3e6abd33f972e
Vyacheslav Nikitin <space.face.ru@gmail.com>
2022-10-26 09:46:09 +0300

    #3 created timer app

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -4,19 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.preference.PreferenceManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewTimer = findViewById(R.id.textViewTimer);
+        CountDownTimer timer = new CountDownTimer(6000, 1000) {
+            @Override
+            public void onTick(long l) {
+                int seconds = (int) (l / 1000);
+                seconds++;
+                textViewTimer.setText(Integer.toString(seconds));
+            }
 
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        preferences.edit().putInt("test", 5).apply();
-        int test = preferences.getInt("test", 0);
-        Toast.makeText(this, Integer.toString(test), Toast.LENGTH_SHORT).show();
+            @Override
+            public void onFinish() {
+                Toast.makeText(MainActivity.this, "Таймер завершил работу", Toast.LENGTH_SHORT).show();
+                textViewTimer.setText(Integer.toString(0));
+            }
+        };
+        timer.start();
     }
 }
